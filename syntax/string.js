3e26e79266e26f23b1fd2f28f01f1bbb7860f1fe
poohcz1 <zergkdl@naver.com>
2020-10-07 18:03:01 +0900

    2day

---
+++ syntax/string.js
@@ -0,0 +1,5 @@
+console.log("1"+"1");
+var str = "문자(char)는 'a'처럼 글자가 하나만 있는 상태를 뜻하고 문자열(char *)은"
+var counting = str.length;
+console.log(counting);
+console.log("문자(char)는 'a'처럼 글자가 하나만 있는 상태를 뜻하고 문자열(char *)은".length);
